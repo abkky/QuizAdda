923e9f7d6d52f9fd5bb085579b9279fdb8de126f
Abhishek kumar Yadav <aky.abhishekkumaryadav@gmail.com>
2021-05-07 12:09:24 +0530

    Login and signup
    yes

--- educator/views.py
+++ educator/views.py
@@ -5,10 +5,21 @@ from django.contrib.auth.models import User,auth
 
 # Create your views here.
 def index(request):
-    return render(request,"eindex.html")
+    if request.user.is_authenticated:
+        return render(request,"eindex.html")
+    else:
+        return redirect("login")
 def login(request):
     if request.method == 'POST':
-        pass
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user= auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/educator")
+        else:
+            messages.info(request,"Invalid Credentials")
+            return redirect("login")
     else:
         return render(request,"login.html")
 def signup(request):
@@ -27,7 +38,10 @@ def signup(request):
         else:
             user= User.objects.create_user(username=username,password=password,email=email,first_name=firstname,last_name=lastname)
             user.save()
-            print('User Created')
+            return redirect("login")
         return redirect("/")
     else:
         return render(request,"signup.html")
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
